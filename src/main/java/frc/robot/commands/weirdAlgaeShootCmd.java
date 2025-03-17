d11f035884a74534c593f5f1105e8be134772c40
ziggytron <ziggytron565@gmail.com>
2025-03-17 09:34:06 -0400

    algae hold + shoot test

---
+++ src/main/java/frc/robot/commands/weirdAlgaeShootCmd.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.kAlgaeRemover.kPivot.RemoverPositions;
+import frc.robot.Constants.kElevator.ElevatorPosition;
+import frc.robot.subsystems.Elevator.Elevator;
+import frc.robot.subsystems.Elevator.AlgaeRemover.AlgaeRemoverPivot;
+import frc.robot.subsystems.Elevator.AlgaeRemover.AlgaeRemoverRollers;
+import frc.robot.subsystems.Elevator.Coral.Coral;
+import frc.robot.subsystems.LEDSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class weirdAlgaeShootCmd extends SequentialCommandGroup {
+  /** Creates a new removeAlgae. */
+  public weirdAlgaeShootCmd(AlgaeRemoverPivot algaeRemoverPivot, AlgaeRemoverRollers algaeRemoverRollers, ElevatorPosition setPos, Elevator elevator, LEDSubsystem ledSubsystem, Coral coral) {
+    addRequirements(algaeRemoverPivot, algaeRemoverRollers, elevator); // Add the required subsystems here
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    addCommands(
+      new SetElevatorHeightCmd(ElevatorPosition.L3, elevator, coral, ledSubsystem),
+      new InstantCommand(() -> elevator.isHoming = true), // TODO: set positions
+      new InstantCommand(() -> elevator.setDutyCycle(.5)),
+      new ParallelCommandGroup(
+        new InstantCommand(() -> algaeRemoverPivot.setPositionTrapazoidal(RemoverPositions.Fling)), // TODO: set positions
+        new InstantCommand(() -> algaeRemoverRollers.setRemoverRollerSpeed(.2)), // TODO: set speed
+        new InstantCommand(() -> coral.setOuttakeSpeedDuty(-.5)))
+          .withDeadline(new WaitCommand(.5)),
+      new InstantCommand(() -> elevator.isHoming = false) // Stop homing after the command is done
+      );
+  }
+}
