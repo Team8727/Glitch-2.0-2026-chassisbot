297b35ceb7a62e0f4395d6ba6cfb16d6f43a5c98
Varun Nanjundappa <naanuboy123@gmail.com>
2026-01-17 22:17:48 -0500

    SwerveRequest and Trigger Command to enable automatic pointing towards target while moving in any direction + invert right side drive motors to drive properly

--- src/main/java/frc/robot/controller/CTReSwerveControls.java
+++ src/main/java/frc/robot/controller/CTReSwerveControls.java
@@ -4,8 +4,6 @@ import Glitch.Lib.NetworkTableLogger;
 import com.ctre.phoenix6.swerve.SwerveModule;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.math.geometry.*;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -33,6 +31,20 @@ public class CTReSwerveControls {
         .withRotationalDeadband(MaxAngularRate * 0.1)
         .withDriveRequestType(SwerveModule.DriveRequestType.OpenLoopVoltage);
 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=- Swerve Request constants to face a fixed target at (targetX, targetY) -=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    Pose3d targetsPose = new Pose3d(
+      new Translation3d(10, 4.5, 1.8),
+      new Rotation3d());
+    // Calculate desired heading in radians
+    final SwerveRequest.FieldCentricFacingAngle faceTarget =
+      new SwerveRequest.FieldCentricFacingAngle()
+        .withDeadband(MaxSpeed)
+        .withRotationalDeadband(MaxAngularRate)
+        .withDriveRequestType(SwerveModule.DriveRequestType.OpenLoopVoltage)
+        .withHeadingPID(50, 0, 0); // P, I, D values for heading control - can probably be tuned with SysId later
+
+    final NetworkTableLogger netLogger = new NetworkTableLogger("CTReSwerveControls");
+
     // Note that X is defined as forward according to WPILib convention,
     // and Y is defined as to the left according to WPILib convention.
     drivetrain.setDefaultCommand(
@@ -43,7 +55,6 @@ public class CTReSwerveControls {
       ));
 
     final Telemetry logger = new Telemetry(MaxSpeed);
-    final NetworkTableLogger netLogger = new NetworkTableLogger("CTReSwerveControls");
 
     // Idle while the robot is disabled. This ensures the configured
     // neutral mode is applied to the drive motors while disabled.
@@ -57,6 +68,35 @@ public class CTReSwerveControls {
     controller.start().and(controller.b()).toggleOnTrue(drivetrain.applyRequest(() -> brake));
     controller.start().and(controller.rightTrigger()).toggleOnTrue(drivetrain.applyRequest(() ->
       point.withModuleDirection(new Rotation2d(-controller.getLeftY(), -controller.getLeftX()))));
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=- Trigger Command to face a fixed target at (targetX, targetY) -=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    controller.a().whileTrue(drivetrain.applyRequest(() -> {
+      //netLogger.logString("CTReSwerveControls", "Face Target Command Activated");
+      // TEMPORARY!!!: Will need the pose that includes vision measurements added for more accuracy
+      Pose2d robotPose = drivetrain.getState().Pose;
+
+      // Calculate vector and heading from robot to target
+      double dx = targetsPose.getX() - robotPose.getX();
+      double dy = targetsPose.getY() - robotPose.getY();
+
+      // Desired heading calculation
+      double desiredHeadingRadians = Math.atan2(dx, dy);
+
+      // Log targetRobotPose for debugging and testing its accuracy
+      Pose2d targetRobotPose = new Pose2d(
+        robotPose.getTranslation(),
+        new Rotation2d((0.5*Math.PI) - desiredHeadingRadians) // 90-degree offset I had to add for the Pose2d targetRobotPose to point correctly
+      );
+
+      //netLogger.logDouble("desiredHeading: ", desiredHeadingRadians);
+      netLogger.logPose3d("targetsPose: ", targetsPose);
+      netLogger.logPose2d("targetRobotPose: ", targetRobotPose);
+
+      return faceTarget
+        .withTargetDirection(Rotation2d.fromRadians((1.5*Math.PI) - desiredHeadingRadians)) // face the target with 180-degree offset I had to add for some reason
+        .withVelocityX(-controller.getLeftY() * MaxSpeed) // translate across field (driving from red to blue alliance sides)
+        .withVelocityY(-controller.getLeftX() * MaxSpeed); // translate across field (driving from field long wall to other long wall)
+    }));
 
     // Run SysId routines when holding back/start and X/Y.
     // Note that each routine should be run exactly once in a single log.
