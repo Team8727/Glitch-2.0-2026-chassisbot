810fd877bba25f146ce42642e3a4f1a6795e1adf
Varun Nanjundappa <naanuboy123@gmail.com>
2026-01-17 22:17:48 -0500

    SwerveRequest and Trigger Command to enable automatic pointing towards target while moving in any direction + invert right side drive motors to drive properly

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import com.pathplanner.lib.commands.PathfindingCommand;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
