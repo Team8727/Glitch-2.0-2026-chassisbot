58f1564354b52ff582ad2cfb92d6d7d344e67272
GriffinGryffinGryphon <griffinlowings@gmail.com>
2026-02-19 17:17:30 -0500

    A command to spin the intake rollers only when the intake is down.

---
+++ src/main/java/frc/robot/Commands/SpinIntake.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Subsystems.IntakePivot;
+import frc.robot.Subsystems.IntakeRoller;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class SpinIntake extends Command {
+
+  private final IntakeRoller roller;
+  private final IntakePivot pivot;
+
+  boolean ended = false;
+
+  /** Creates a new IntakeRollerCommand. */
+  public SpinIntake(IntakeRoller roller, IntakePivot pivot) {
+
+    this.roller = roller;
+    this.pivot = pivot;
+
+    addRequirements(roller, pivot);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (pivot.getPosition() != IntakePivot.IntakePosition.DOWN.getDegrees()) {
+      roller.setSpeedDutyCycle(0);
+    } else {
+      roller.setSpeedDutyCycle(1);
+    }
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    roller.setSpeedDutyCycle(0);
+    ended = true;
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return ended;
+  }
+}
