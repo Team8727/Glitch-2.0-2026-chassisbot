d2fb2a4bca6bca868e36fabc111a6dd5f8da3a27
Varun Nanjundappa <naanuboy123@gmail.com>
2026-02-19 16:48:55 -0500

    IndexAndShoot

---
+++ src/main/java/frc/robot/Commands/IndexAndShoot.java
@@ -0,0 +1,28 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class IndexAndShoot extends Command {
+
+  @Override
+  public void initialize() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void execute() {
+    // This method will be called repeatedly until this Command either finishes or is interrupted.
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    // This method will be called once after isFinished returns true, or if the command is interrupted/canceled.
+  }
+
+  @Override
+  public boolean isFinished() {
+    // Return true when this Command no longer needs to run execute(). Returning false will result in this Command being scheduled forever.
+    return false;
+  }
+
+}
