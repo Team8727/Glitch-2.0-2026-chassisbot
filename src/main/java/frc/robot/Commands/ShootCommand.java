f2aca1a5a4c21ac97cc37d5dd96a73f9ee3a5668
Varun Nanjundappa <naanuboy123@gmail.com>
2026-04-06 16:35:46 -0400

    Combined ShootCommand (fixedVel needs to be made into Optional)

--- src/main/java/frc/robot/Commands/ShootCommand.java
+++ src/main/java/frc/robot/Commands/ShootCommand.java
@@ -1,34 +1,53 @@
 package frc.robot.Commands;
 
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
 import frc.robot.Subsystems.Indexer;
-import frc.robot.Subsystems.LEDSubsystem;
 import frc.robot.Subsystems.ShooterRoller;
 
 import static edu.wpi.first.wpilibj2.command.Commands.*;
 
 public class ShootCommand extends SequentialCommandGroup {
-  public ShootCommand(Indexer indexer, ShooterRoller shooterRoller, LEDSubsystem leds) {
+  public ShootCommand(Indexer indexer, ShooterRoller shooterRoller, double fixedLinearVelocity, ControlMode controlMode) {
     addCommands(
             parallel(
                     shooterRoller.run(() -> {
-                      double speed;
-                      if (Robot.SHOOT_POWER_OVERRIDE) {
-                        speed = 6.6 * Math.PI * Robot.SHOOTER_LOSS_COMPENSATION; // Old method
+                      double speedCoefficient = 1 / (Math.PI * Robot.SHOOTER_FLYWHEEL_RADIUS_METERS); // Used to convert to rps with multiplier
+                      double speed = speedCoefficient * ((fixedLinearVelocity == 0) ? Robot.firing.power : fixedLinearVelocity); // Whether to use manual shoot override or not
+
+                      if (controlMode == ControlMode.FEEDFORWARD) {
+                        shooterRoller.setFFVoltageWithVelocity(1 * speed);
+                      } else if (controlMode == ControlMode.PID) {
+                        shooterRoller.setSpeedVelocity(1.95 * speed);
                       } else {
-                        //speed = Robot.firing.power * Math.PI * Robot.SHOOTER_LOSS_COMPENSATION; // Old Method
-                        speed = 0.95 * (Robot.firing.power) / (Math.PI * Robot.SHOOTER_FLYWHEEL_RADIUS_METERS);
+                        shooterRoller.m_loop.setNextR(VecBuilder.fill(0.95 * (Robot.firing.power) / (Math.PI * Robot.SHOOTER_FLYWHEEL_RADIUS_METERS))); // In rad/sec
+                        shooterRoller.m_loop.correct(VecBuilder.fill(shooterRoller.getVelocity()));
+                        shooterRoller.m_loop.predict(0.020);
+                        double nextVoltage = shooterRoller.m_loop.getU(0);
+                        shooterRoller.setSpeedVoltage(nextVoltage);
                       }
-                      shooterRoller.setSpeedVelocity(speed);
-                      leds.shootPatterns(shooterRoller.getFlywheelVelocity(), speed);
                     }),
                     sequence(
                             waitSeconds(2),
                             indexer.run(() -> indexer.setSpeedDutyCycle(-.9))
                                     .withTimeout(1)
                     )
-            ).finallyDo(leds::endCommand)//.withTimeout(2.5)
+            ).finallyDo(() -> {
+              if (controlMode == ControlMode.STATE_SPACE) {
+                shooterRoller.m_loop.setNextR(0.95 * (Robot.firing.power) / (Math.PI * Robot.SHOOTER_FLYWHEEL_RADIUS_METERS));
+                shooterRoller.m_loop.correct(VecBuilder.fill(shooterRoller.getVelocity()));
+                shooterRoller.m_loop.predict(0.020);
+                double nextVoltage = shooterRoller.m_loop.getU(0);
+                shooterRoller.setSpeedVoltage(nextVoltage);
+              }
+            })
     );
+  }
+
+  public enum ControlMode {
+    FEEDFORWARD,
+    PID,
+    STATE_SPACE
   }
 }
